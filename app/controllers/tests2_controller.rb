4a27b9fe6eddea89d102ebf89a9fcc1ddb98883a
yujimiya <yuji.miyamoto32@gmail.com>
2023-09-11 20:16:40 +0900

    tests2コントローラーの作成

---
+++ app/controllers/tests2_controller.rb
@@ -0,0 +1,4 @@
+class Tests2Controller < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/tests2_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Tests2ControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
